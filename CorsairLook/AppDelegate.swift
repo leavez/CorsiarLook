b1dbb6f20c8089291afa374632450f32d2ba1c50
leave Gao <gaojiji@gmail.com>
2018-11-03 18:56:11 +0800

    Initial Commit

---
+++ CorsairLook/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  CorsairLook
+//
+//  Created by Leave Gao on 2018/11/3.
+//  Copyright © 2018 me.leavez. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
